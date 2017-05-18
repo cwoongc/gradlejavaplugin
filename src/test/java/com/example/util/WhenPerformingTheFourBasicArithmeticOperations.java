b2cb87d14abc21141a644547c7c18f91bf6d5af3
Woong-Chul, Choi <switch.viewpoint@gmail.com>
2017-05-18 11:39:45 +0900

    first commit

---
+++ src/test/java/com/example/util/WhenPerformingTheFourBasicArithmeticOperations.java
@@ -0,0 +1,20 @@
+package com.example.util;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+
+import java.math.BigDecimal;
+import static org.junit.Assert.*;
+
+@RunWith(Enclosed.class) 
+public class WhenPerformingTheFourBasicArithmeticOperations {
+    
+    public static class InAddition {
+        @Test
+        public void shouldReturnTheSumOfNumbersWhichAreNumericString() {
+            assertEquals(new BigDecimal(2), BasicArithmeticOperations.plus("1","1"));
+        }
+    }
+}
